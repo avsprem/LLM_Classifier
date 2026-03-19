5288e2d90957d765178b489785e816e708a0f696
Akila Premathilaka <127757047+avsprem@users.noreply.github.com>
2026-03-19 13:32:58 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,7 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = "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"
+
+# MODEL = "openai/gpt-5.2"   # ✅ cheaper model
+# MAX_WORKERS = 5
 
 MODEL = "gpt-5-mini"
 MAX_WORKERS = 5
